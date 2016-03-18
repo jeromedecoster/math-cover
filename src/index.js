c12527270d559d1ce2c76f81d3bf06a967f659d2
jeromedecoster <github@jeromedecoster.com>
2016-03-18 16:08:37 +0100

    gh-pages

--- src/index.js
+++
@@ -1,6 +0,0 @@
-
-const Demo = require('./components/Demo')
-const ReactDOM = require('react-dom')
-const React = require('react')
-
-ReactDOM.render(<Demo/>, document.querySelector('[react-demo]'))
